820e9a71bf949120043ad57d53891dbdde5d0c8d
eventic05 <soporteeventic@gmail.com>
2018-09-04 21:13:27 -0500

    4 de septiembre

--- app/config/routes.rb
+++ app/config/routes.rb
@@ -5,5 +5,10 @@ Rails.application.routes.draw do
   resources :posts
   resources :images
   root "welcome#index"
+  namespace 'api' do
+    namespace 'v1' do
+      post 'auth_user' => 'authentication#authenticate_user'
+    end
+  end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
